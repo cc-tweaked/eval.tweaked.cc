ebce13ece08351dcfa9df3cf408fd5a3c8ce50c5
Jonathan Coates <git@squiddev.cc>
2022-06-10 00:39:10 +0100

    Bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ repositories {
     maven("https://squiddev.cc/maven")
 }
 
-val modVersion = "1.100.3"
+val modVersion = "1.100.6"
 
 dependencies {
     implementation("org.squiddev:cc-tweaked-1.16.5:${modVersion}")
