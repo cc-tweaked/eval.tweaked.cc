f5163176a354daa718b0d0b1cb4e9b1ae9b13ec3
Jonathan Coates <git@squiddev.cc>
2022-07-29 20:41:33 +0100

    Bump CC:T version to 1.100.9

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ repositories {
     maven("https://squiddev.cc/maven")
 }
 
-val modVersion = "1.100.8"
+val modVersion = "1.100.9"
 
 dependencies {
     implementation("org.squiddev:cc-tweaked-1.16.5:${modVersion}")
