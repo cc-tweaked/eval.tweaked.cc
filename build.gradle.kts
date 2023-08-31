cc19dea0e70ccc8d45a262ead267249fefd607cc
Jonathan Coates <git@squiddev.cc>
2023-08-31 10:13:22 +0100

    Switch to logback for logging

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,11 +29,11 @@ dependencies {
     implementation("org.slf4j:slf4j-api:2.0.0")
     implementation("com.google.guava:guava:31.1-jre")
 
+    // Instrumentation
     val otVersion = "1.29.0"
     implementation(platform("io.opentelemetry:opentelemetry-bom:$otVersion"))
     implementation(platform("io.opentelemetry:opentelemetry-bom-alpha:$otVersion-alpha"))
 
-    // Tracing
     implementation("io.opentelemetry:opentelemetry-api")
     implementation("io.opentelemetry:opentelemetry-api")
     implementation("io.opentelemetry:opentelemetry-sdk")
@@ -41,10 +41,11 @@ dependencies {
     implementation("io.opentelemetry:opentelemetry-extension-trace-propagators")
     implementation("io.opentelemetry:opentelemetry-semconv")
 
-    runtimeOnly("org.apache.logging.log4j:log4j-core:2.19.0")
-    runtimeOnly("io.opentelemetry.instrumentation:opentelemetry-log4j-context-data-2.17-autoconfigure:$otVersion-alpha")
-    runtimeOnly("io.grpc:grpc-netty:1.40.1")
-    runtimeOnly("org.apache.logging.log4j:log4j-slf4j2-impl:2.19.0")
+    runtimeOnly("io.opentelemetry.instrumentation:opentelemetry-logback-mdc-1.0:$otVersion-alpha")
+
+    // Logging
+    runtimeOnly("ch.qos.logback:logback-core:1.4.11")
+    runtimeOnly("ch.qos.logback:logback-classic:1.4.11")
 }
 
 application {
@@ -66,7 +67,7 @@ tasks.named<Jar>("jar") {
 tasks.named<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar>("shadowJar") {
     mergeServiceFiles()
     minimize() {
-        exclude(dependency("org.apache.logging.log4j:.*:.*"))
+        exclude(dependency("ch.qos.logback:.*:.*"))
         exclude(dependency("io.opentelemetry.*:.*:.*"))
     }
 }
